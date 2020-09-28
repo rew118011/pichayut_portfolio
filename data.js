add5a541fc11b8b5e9cbfdd24b2a77dcb3f5ca43
toeytoey <614259012@webmail.npru.ac.th>
2020-09-28 14:22:13 +0700

    Initial portfolio project Commit

---
+++ data.js
@@ -0,0 +1,53 @@
+const data = {
+    "fullname" : "Mr. Pichayut Intharat",
+    "nickname" : "REW",
+    "motto" : "When you stop advancing, you’ve already begun to retreat.",
+    "work" : "Student at Software Engineering Program in Nakhon Pathom Rajabhat University",
+    "aboutme" : {
+        "name" : "Mr.Pichayut Intharat",
+        "nickname" : "Rew",
+        "sex" : "Male",
+        "education" : "Songphinongwittaya School",
+        "date" : "Sunday 31st Janury 1999",
+        "age" : "21 years",
+        "nationality" : "Thai",
+        "race" : "Thai",
+        "religion" : "Buddhism",
+        "bloodgroup" : "B",
+        "address" : "70 Moo.1 lamlang kabure Ranong 85110",
+        "phone" : "0640848827",
+        "email" : "614259040@webmail.npru.ac.th",
+        "characteristic" : "Talk openly",
+        "hobbies" : "Listen to music, See movie, Betta fish farming, Learn more about your interests.",
+        "sport" : "Futsal",
+        "pets" : "dog",
+        "attitude" : "Positive",
+        "motto" : "When you stop advancing, you’ve already begun to retreat."
+    },
+    "education" : {
+        "education1" : "kabure School",
+        "education2" : "kabure School",
+        "education3" : "kabure School",
+        "education4" : "Nakhon Pathom Rajabhat University"
+    },
+    "experience" : {
+        "experience1" : "none"
+    },
+    "skill" : {
+        "html" : "Level-Basic",
+        "css" : "Level-Basic",
+        "php" : "Level-Basic",
+        "java" : "Level-Basic",
+        "javascript" : "Level-Basic",
+        "sql" : "Level-Basic",
+        "json" : "Level-Basic"
+    },
+    "contact" : {
+        "fb" : "Pichayut Intharat",
+        "ig" : "PichayutIntharat",
+        "line" : "pichayut.123",
+        "email" : "614259040@webmail.npru.ac.th",
+        "phone" : "0640848827"
+    }
+};
+module.exports = data;
